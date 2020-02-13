2e2e0c7a4c704aff530f95b4b21540d8243649b3
Paul K <pkfis@outlook.com>
2020-02-13 01:42:59 +0000

    Update file system

--- db/migrate/20200213012612_create_build_entries.rb
+++ db/migrate/20200213012612_create_build_entries.rb
@@ -1,4 +1,11 @@
 class CreateBuildEntries < ActiveRecord::Migration
   def change
+    create_table :build_entries do |t|
+      t.string :title
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
   end
 end
